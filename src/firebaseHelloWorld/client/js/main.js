cc4ae8a53ccb14211e0e6fbed19c72389f9e5bd3
Vladimir Prokopovich <vprokopovich@gmail.com>
2015-01-24 18:57:46 +0300

    Implemented basic client app without GUI for FIrebase Hello World

--- src/firebaseHelloWorld/client/js/main.js
+++ src/firebaseHelloWorld/client/js/main.js
@@ -1,11 +1,11 @@
-var requestIds = [];
 var requests = new Object();
 var msgPool = null;
 var state = null;
 
-$(document).ready(function() {
+$(document).ready(function()
+{
     msgPool = new Firebase('https://heater-contol.firebaseio.com/msg-pool/');
-    state = new Firebase('https://heater-contol.firebaseio.com/state/');
+    state   = new Firebase('https://heater-contol.firebaseio.com/state/');
 
     state.on('value', handleStateChanged);
     msgPool.on('child_removed', handleMsgResponse);
@@ -24,15 +24,14 @@ function handleStateChanged(snapshot)
 
 function handleMsgResponse(oldChildSnapshot)
 {
-    requests[oldChildSnapshot.key()] = null;
-    $("div#lblRequests").html(JSON.stringify(requests));
+    delete requests[oldChildSnapshot.key()];
+    updateViewProcessingRequests();
 }
 
 function handleMsgRequest(childSnapshot, prevChildName)
 {
-    //$("body").append("<div>"+JSON.stringify(childSnapshot.key())+"___" + prevChildName+"</div>");
     requests[childSnapshot.key()] = childSnapshot.val();
-    $("div#lblRequests").html(JSON.stringify(requests));
+    updateViewProcessingRequests();
 }
 
 function updateView(status, temperature, targetTemperature, targetTime, timerActivated)
@@ -43,10 +42,27 @@ function updateView(status, temperature, targetTemperature, targetTime, timerAct
     $("input#txtTargetTime").val(targetTime);
 }
 
+function updateViewProcessingRequests()
+{
+    if (false == isEmpty(requests))
+    {
+        $("div#lblRequestStatus").html("Processing request");
+        $("div#lblRequests").html(JSON.stringify(requests));
+    }
+    else
+    {
+        $("div#lblRequestStatus").html("All requests processed");
+        $("div#lblRequests").html("");
+    }    
+}
+
 function onTargetTemperatureChange()
 {
     var newTemperature = $("input#txtTargetTemp").val();
-    var newMessageRef = msgPool.push({'action': 'targetTemp', 'value': newTemperature});
-    var path = newMessageRef.toString();
-    requestIds.push(path);
+    msgPool.push({'action': 'targetTemp', 'value': newTemperature});
+}
+
+function isEmpty(obj)
+{
+    return Object.keys(obj).length === 0;
 }
