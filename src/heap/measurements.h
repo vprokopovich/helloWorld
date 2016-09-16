f175345bcfb0c3214a21b54ca576f27cb1c9a353
VProkopovich <vprokopovich@gmail.com>
2016-09-16 10:00:44 +0300

    Fixed measurement: added initialization

--- src/heap/measurements.h
+++ src/heap/measurements.h
@@ -7,14 +7,14 @@ namespace Util
     Measure(F f, T& inputData, std::size_t count = 100)
     {
         std::chrono::duration<std::chrono::high_resolution_clock::rep,
-                              std::chrono::high_resolution_clock::period> retVal;
+                              std::chrono::high_resolution_clock::period> retVal(0);
         for (std::size_t i = 0; i < count; i++)
         {
             auto dataToProcess = inputData;
             auto begin = std::chrono::high_resolution_clock::now();
-            
+
             f(dataToProcess);
-            
+
             auto end = std::chrono::high_resolution_clock::now();
             retVal += end - begin;
         }
