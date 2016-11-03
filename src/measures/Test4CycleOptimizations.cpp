7dc91cfe39a89f282fa2e71b5e6d187a65d0318c
VProkopovich <vprokopovich@gmail.com>
2016-11-03 11:35:41 +0200

    Added implementation of test 'cycle optimization'

--- src/measures/Test4CycleOptimizations.cpp
+++ src/measures/Test4CycleOptimizations.cpp
@@ -24,34 +24,67 @@ void Test4CycleOptimizations::Run(std::size_t testRepetitions)
     using ArgType = std::uint64_t;
 
     const std::size_t numberIterations = std::numeric_limits<std::uint32_t>::max() / (1024);
-    const std::size_t valuesCount = std::numeric_limits<std::uint32_t>::max() / 1024;
+    const std::size_t valuesCount =  std::numeric_limits<std::uint32_t>::max() / 512 + 1;
     std::vector<ArgType> inputData(valuesCount);
     std::generate(inputData.begin(), inputData.end(), std::rand);
 
     {
-        bool isTrue = true;
-        auto test = [&inputData, &valuesCount, &isTrue] (const ArgType& count) -> void
+        auto test = [&inputData, &valuesCount] (const ArgType& count) -> void
         {
-            // Some code
-            std::uint64_t counter = 0;
-
-            if (isTrue)
+            ArgType res = 0;
+            for (std::size_t i = 0; i < valuesCount; i++)
             {
-                for (auto i = 0; i < count; ++i)
-                {
-                    counter++;
-                }
+                res *= inputData[i];
             }
-            else
-            {
-                for (auto i = 0; i < count; ++i)
-                {
-                    counter++;
-                }
-            }
+            std::cout << "Res1" << res << std::endl;
         };
 
         auto averageDuration = Util::Measure(test, numberIterations, testRepetitions);
-        Util::Print(averageDuration, generateOutputString("Smth, ", numberIterations));
+        Util::Print(averageDuration, generateOutputString("Cycle, ", numberIterations));
+    }
+    {
+        auto testUnfolded1 = [&inputData, &valuesCount](const ArgType& count) -> void
+        {
+            ArgType res = 0;
+            ArgType* pInputData = &inputData[0];
+            for (std::size_t i = 0; i < valuesCount; i += 4)
+            {
+                /*
+                res *= *pInputData++;
+                res *= *pInputData++;
+                res *= *pInputData++;
+                res *= *pInputData++;
+                */
+                res *= inputData[i];
+                res *= inputData[i + 1];
+                res *= inputData[i + 2];
+                res *= inputData[i + 3];
+            }
+            std::cout << "Res2" << res << std::endl;
+        };
+
+        auto averageDuration = Util::Measure(testUnfolded1, numberIterations, testRepetitions);
+        Util::Print(averageDuration, generateOutputString("Unfolded cycle #1, ", numberIterations));
+    }
+    {
+        auto testUnfolded2 = [&inputData, &valuesCount](const ArgType& count) -> void
+        {
+            ArgType res1 = 0;
+            ArgType res2 = 0;
+            ArgType res3 = 0;
+            ArgType res4 = 0;
+            for (std::size_t i = 0; i < valuesCount; i += 4)
+            {
+                res1 *= inputData[i];
+                res2 *= inputData[i + 1];
+                res3 *= inputData[i + 2];
+                res4 *= inputData[i + 3];
+            }
+            ArgType res = res1 * res2 * res3 * res4;
+            std::cout << "Res3" << res << std::endl;
+        };
+
+        auto averageDuration = Util::Measure(testUnfolded2, numberIterations, testRepetitions);
+        Util::Print(averageDuration, generateOutputString("Unfolded cycle #2, ", numberIterations));
     }
 }
