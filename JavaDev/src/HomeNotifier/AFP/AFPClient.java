bea42515759c10bc38837eeae1838d4e8bc58cd9
VProkopovich <vprokopovich@gmail.com>
2016-12-28 23:06:09 +0200

    [HomeNotifier] Added AFPClient. Getting webpage implemented

---
+++ JavaDev/src/HomeNotifier/AFP/AFPClient.java
@@ -0,0 +1,107 @@
+package HomeNotifier.AFP;
+
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class AFPClient
+{
+	public enum Mark
+	{
+		Toyota(116),
+		Mazda(76),
+		Honda(53),
+		Hyundai(56),
+		Kia(62),
+		Mitsubishi(83),
+		Skoda(105);
+		
+		Mark(int value) {this._value = value;}
+		int ToInt(){return _value;}
+		private int _value;
+	}
+	public enum Region
+	{
+		Odessa(81),
+		Kyiv(76),
+		Chernigov(90),
+		Nikolaev(80);
+		
+		Region(int value) {this._value = value;}
+		int ToInt(){return _value;}
+		private int _value;
+	}
+	
+	public String GetWebPage(Mark mark, Region region)
+	{
+		String retVal = "";
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+        
+		try
+		{
+            HttpGet httpGet = new HttpGet(GenerateUrl(mark, region));	// originally was: "http://httpbin.org/get"
+            
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>()
+            {
+                @Override
+                public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException
+                {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300)
+                    {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    }
+                    else
+                    {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+            };
+            
+            retVal = httpclient.execute(httpGet, responseHandler);
+        }
+		catch (ClientProtocolException e)
+		{
+			// TODO Write log with error
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			// TODO Write log with error
+			e.printStackTrace();
+		}
+        finally
+        {
+            try
+            {
+				httpclient.close();
+			}
+            catch (IOException e)
+            {
+				// TODO Write log with error
+				e.printStackTrace();
+			}
+        }
+		
+		return retVal;
+	}
+
+	// Some helpers
+	private final String _baseUrl = "http://www.afp.com.ua/sale/?rub=1&model=&yearfrom=&yearto=&pricefrom=&priceto=&moneyid=93";
+	private String GenerateUrl(Mark mark, Region region)
+	{
+		// Adding following things to base url: &region=81&mark=116
+		final String retVal = _baseUrl 
+		                    + "&region=" + region.ToInt()
+		                    + "&mark=" + mark.ToInt();
+		return retVal;
+	}
+}
--- JavaDev/src/HomeNotifier/HomeNotifier.java
+++ JavaDev/src/HomeNotifier/HomeNotifier.java
@@ -1,11 +1,25 @@
 package HomeNotifier;
 
-public class HomeNotifier {
+import HomeNotifier.AFP.AFPClient;
+
+public class HomeNotifier
+{
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, World");
+		
+		// TODO: !! add .ini files parser
+		// TODO: !! add .csv files parser
+		// TODO: !! add support of command line options
+		// TODO: !! add XML parser
+
+		
+		AFPClient afp = new AFPClient();
+		String sWebpage = afp.GetWebPage(AFPClient.Mark.Toyota, AFPClient.Region.Odessa);
+		
+		System.out.println(sWebpage);
 	}
 }
