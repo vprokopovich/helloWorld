cd4c74a47e9a2b009f85909c0b5b0ab20f804b88
Vladimir Prokopovich <vlpr@kyiv.ciklum.net>
2016-12-28 13:03:45 +0200

    Added HomeNotifier init directory

---
+++ JavaDev/src/HomeNotifier.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author vlpr
+ *
+ */
+public class HomeNotifier {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, World");
+	}
+
+}
